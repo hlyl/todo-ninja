ed04ca1f9fc34fa44e04bab1a75e439a43b8c12c
hlyl <hlyl@novonordisk.com>
2022-06-04 06:03:35 +0000

    NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar flat color="grey lighten-3">
+    <v-toolbar-title flat class="text-uppercase grey--text">
+      <span class="font-weight-light">dev</span>
+      <span>Todo</span>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text color="grey">
+      <span>Sign Out</span>
+
+      <v-icon right>mdi-exit-to-app</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
